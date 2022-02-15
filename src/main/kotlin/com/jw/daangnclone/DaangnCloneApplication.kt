c1e1af648e483fd3ddcad2b957a0a066c1a1886f
JaeWoong.Lee <59162713+JaeWoong-L@users.noreply.github.com>
2022-02-15 10:26:06 +0900

    Initial commit

---
+++ src/main/kotlin/com/jw/daangnclone/DaangnCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.jw.daangnclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaangnCloneApplication
+
+fun main(args: Array<String>) {
+	runApplication<DaangnCloneApplication>(*args)
+}
